af7cea3baeed2bda68700d26aaae1bbf1d0710f7
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-05-25 13:52:50 +1000

    imgs

---
+++ controllers/prescriptions.js
@@ -0,0 +1,30 @@
+const Performer = require('../models/performer');
+const Movie = require('../models/movie');
+
+module.exports = {
+  new: newPrescription,
+  create,
+  addToCast
+};
+
+async function addToCast(req, res) {
+  const patient = await Patient.findById(req.params.id);
+  movie.prescription.push(req.body.performerId);
+  await movie.save();
+  res.redirect(`/movies/${movie._id}`);
+}
+
+async function newPrescription(req, res) {
+  const performers = await Performer.find({}).sort('name');
+  res.render('performers/new', { title: 'Add Performer', performers });
+}
+
+async function create(req, res) {
+
+  try {
+    await Performer.create(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect('/performers/new');
+}
